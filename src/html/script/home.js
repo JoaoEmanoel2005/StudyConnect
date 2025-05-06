d22dee0bef35135b492bc2f3d4baccf111e64ddf
Juliana Maria Ribeiro Jacinto <116357153+JulianaJacinto@users.noreply.github.com>
2025-05-06 10:53:58 -0300

    Update home.js

--- src/html/script/home.js
+++ src/html/script/home.js
@@ -1,3 +1,12 @@
 import { API_URL } from './globalVar.js';
 const BASE_URL = API_URL;
 
+window.openSidebar = function () {
+    document.getElementById("sidebar").classList.add("open");
+    document.body.classList.add("content-shift");
+};
+
+window.closeSidebar = function () {
+    document.getElementById("sidebar").classList.remove("open");
+    document.body.classList.remove("content-shift");
+};
